d1b21b804d0941dcded0368efa84443b24e5c78f
Alejandro Herrera <brayan.herrera@utp.edu.co>
2021-11-17 20:45:13 -0500

    Se documenta el archivo LiderSuma

--- Cliente1.py
+++ Cliente1.py
@@ -1,17 +1,19 @@
 import socket
 
-TCP_IP = 'localhost'
+TCP_IP = "localhost"
 TCP_PORT = 5004
 BUFFER_SIZE = 1024
 
+
 def lee_entero():
     while True:
-       entrada = input("Escribe un numero entero: ")
-       try:
-           entrada = int(entrada)
-           return entrada
-       except ValueError:
-           print("La entrada es incorrecta: escribe un numero entero")
+        entrada = input("Escribe un numero entero: ")
+        try:
+            entrada = int(entrada)
+            return entrada
+        except ValueError:
+            print("La entrada es incorrecta: escribe un numero entero")
+
 
 def listar(dic, crear):
     if dic.__contains__(crear):
@@ -39,12 +41,15 @@ def listar(dic, crear):
     elif True:
         print("El grupo solicitado no existe...")
 
+
 while True:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((TCP_IP,TCP_PORT))
-    print("Que desea realizar: \n1.Suma\n2.Resta\n3.Multiplicacion\n4.Division\n5.Potencia"+
-        "\n6.Logaritmo\n7.Raiz\n8.Crear servidor\n9.Ingresar servidor a un grupo\n10.Crear Grupo\n11.Eliminar Grupo"+
-        "\n12.Listar Grupos")
+    s.connect((TCP_IP, TCP_PORT))
+    print(
+        "Que desea realizar: \n1.Suma\n2.Resta\n3.Multiplicacion\n4.Division\n5.Potencia"
+        + "\n6.Logaritmo\n7.Raiz\n8.Crear servidor\n9.Ingresar servidor a un grupo\n10.Crear Grupo\n11.Eliminar Grupo"
+        + "\n12.Listar Grupos"
+    )
 
     op = lee_entero()
     if op >= 1 and op <= 7:
@@ -53,13 +58,7 @@ while True:
         print("Digite numero2: ")
         num2 = lee_entero()
         crear = 0
-        datos = {
-            'Operando':op,
-            'Numero1': num1,
-            'Numero2': num2,
-            'CrearS' : crear,
-            'Puerto' : 0
-        }
+        datos = {"Operando": op, "Numero1": num1, "Numero2": num2, "CrearS": crear, "Puerto": 0}
 
         s.send(str(datos).encode("UTF-8"))
         data = s.recv(BUFFER_SIZE).decode("UTF-8")
@@ -68,29 +67,28 @@ while True:
         print("El resultado de la operacion fue: ", total)
     elif op > 7:
         if op == 8:
-            print("Que tipo de servidor desea crear: \n1.Suma\n2.Resta\n3.Multiplicacion\n4.Division\n5.Potencia\n6.Logaritmo\n7.Raiz")
+            print(
+                "Que tipo de servidor desea crear: \n1.Suma\n2.Resta\n3.Multiplicacion\n4.Division\n5.Potencia\n6.Logaritmo\n7.Raiz"
+            )
             crear = lee_entero()
 
             datos = {
-            'Operando':op,
-            'Numero1': 0,
-            'Numero2': 0,
-            'CrearS': crear,
+                "Operando": op,
+                "Numero1": 0,
+                "Numero2": 0,
+                "CrearS": crear,
             }
             s.send(str(datos).encode("UTF-8"))
             data = s.recv(BUFFER_SIZE).decode("UTF-8")
             print("Servidor creado....")
         if op == 9:
             clave = 1
-            print("Ingrese el grupo en donde se encuentra el servidor que desee agregar a un nuevo grupo:\n1.Suma\n2.Resta\n3.Multiplicacion\n4.Division\n5.Potencia\n6.Logaritmo\n7.Raiz ")
+            print(
+                "Ingrese el grupo en donde se encuentra el servidor que desee agregar a un nuevo grupo:\n1.Suma\n2.Resta\n3.Multiplicacion\n4.Division\n5.Potencia\n6.Logaritmo\n7.Raiz "
+            )
             crear = lee_entero()
 
-            datos = {
-            'Operando':op,
-            'Numero1': 0,
-            'Numero2': 0,
-            'CrearS': crear
-            }
+            datos = {"Operando": op, "Numero1": 0, "Numero2": 0, "CrearS": crear}
 
             s.send(str(datos).encode("UTF-8"))
             data = s.recv(BUFFER_SIZE).decode("UTF-8")
@@ -99,49 +97,42 @@ while True:
             aux = len(dic)
             print("seleccione el puerto del servidor al cual desea cambiar de grupo: ")
             while clave < aux:
-                print(str(clave)+":",dic[clave+1])
+                print(str(clave) + ":", dic[clave + 1])
                 clave += 1
 
             op = lee_entero()
-            puerto = dic[op+1]
+            puerto = dic[op + 1]
 
-            print("Indique a que servidor lo quiere unir:\n1.Suma\n2.Resta\n3.Multiplicacion\n4.Division\n5.Potencia\n6.Logaritmo\n7.Raiz ")
+            print(
+                "Indique a que servidor lo quiere unir:\n1.Suma\n2.Resta\n3.Multiplicacion\n4.Division\n5.Potencia\n6.Logaritmo\n7.Raiz "
+            )
             servidor = lee_entero()
 
-            datos = {
-                'Puerto' : puerto,
-                'Servidor' : servidor
-            }
+            datos = {"Puerto": puerto, "Servidor": servidor}
 
             s.send(str(datos).encode("UTF-8"))
             data = s.recv(BUFFER_SIZE).decode("UTF-8")
             print("Se agrego el servidor al nuevo grupo")
 
         elif op == 10:
-            print("Que grupo desea crear: \n1.Suma\n2.Resta\n3.Multiplicacion\n4.Division\n5.Potencia\n6.Logaritmo\n7.Raiz")
+            print(
+                "Que grupo desea crear: \n1.Suma\n2.Resta\n3.Multiplicacion\n4.Division\n5.Potencia\n6.Logaritmo\n7.Raiz"
+            )
             crear = lee_entero()
 
-            datos = {
-            'Operando':op,
-            'Numero1': 0,
-            'Numero2': 0,
-            'CrearS': crear
-            }
+            datos = {"Operando": op, "Numero1": 0, "Numero2": 0, "CrearS": crear}
 
             s.send(str(datos).encode("UTF-8"))
             data = s.recv(BUFFER_SIZE).decode("UTF-8")
             print("Se creo el grupo Exitosamente")
 
         elif op == 11:
-            print("Que grupo desea Eliminar: \n1.Suma\n2.Resta\n3.Multiplicacion\n4.Division\n5.Potencia\n6.Logaritmo\n7.Raiz")
+            print(
+                "Que grupo desea Eliminar: \n1.Suma\n2.Resta\n3.Multiplicacion\n4.Division\n5.Potencia\n6.Logaritmo\n7.Raiz"
+            )
             crear = lee_entero()
 
-            datos = {
-            'Operando':op,
-            'Numero1': 0,
-            'Numero2': 0,
-            'CrearS': crear
-            }
+            datos = {"Operando": op, "Numero1": 0, "Numero2": 0, "CrearS": crear}
 
             s.send(str(datos).encode("UTF-8"))
             data = s.recv(BUFFER_SIZE).decode("UTF-8")
@@ -149,15 +140,12 @@ while True:
 
         elif op == 12:
             bandera = False
-            print("Que grupo desea listar: \n1.Suma\n2.Resta\n3.Multiplicacion\n4.Division\n5.Potencia\n6.Logaritmo\n7.Raiz")
+            print(
+                "Que grupo desea listar: \n1.Suma\n2.Resta\n3.Multiplicacion\n4.Division\n5.Potencia\n6.Logaritmo\n7.Raiz"
+            )
             crear = lee_entero()
 
-            datos = {
-            'Operando':op,
-            'Numero1': 0,
-            'Numero2': 0,
-            'CrearS': crear
-            }
+            datos = {"Operando": op, "Numero1": 0, "Numero2": 0, "CrearS": crear}
 
             s.send(str(datos).encode("UTF-8"))
             data = s.recv(BUFFER_SIZE).decode("UTF-8")
--- LiderResta.py
+++ LiderResta.py
@@ -1,12 +1,12 @@
 import socket
 import random
 
-TCP_IP = 'localhost'
+TCP_IP = "localhost"
 TCP_PORT = 5027
 BUFFER_SIZE = 1024
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP,TCP_PORT))
+s.bind((TCP_IP, TCP_PORT))
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.listen(5)
 
@@ -19,23 +19,18 @@ while True:
     conn.send("Recibido".encode("UTF-8"))
 
     op = str(data.get("Operando"))
-    num1 =  str(data.get("Numero1"))
-    num2 =  str(data.get("Numero2"))
+    num1 = str(data.get("Numero1"))
+    num2 = str(data.get("Numero2"))
     dic = dict(data.get("Servidores"))
 
-    servidorElegido = random.randint(2,len(dic))
+    servidorElegido = random.randint(2, len(dic))
     print("El servidor elegido fue el puerto: ", dic.get(servidorElegido))
 
     s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s2.connect((TCP_IP,dic.get(servidorElegido)))
+    s2.connect((TCP_IP, dic.get(servidorElegido)))
     s2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    datos = {
-        'Operando':op,
-        'Numero1': num1,
-        'Numero2': num2,
-        'Servidores':dic
-    }
+    datos = {"Operando": op, "Numero1": num1, "Numero2": num2, "Servidores": dic}
     s2.send(str(datos).encode("UTF-8"))
     print("Datos enviados ", datos)
     total = s2.recv(BUFFER_SIZE).decode("UTF-8")
